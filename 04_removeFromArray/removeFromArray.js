1be49bdef7ab2c293b3a6e49352fc1a960795f16
chagstep1 <yandex@chagstep1.ru>
2021-12-27 16:02:41 +0300

    Complete Exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array) {
+	let new_array = array
+	for (let i = 1; i < arguments.length; ++i)
+	{
+		let index = new_array.indexOf(arguments[i]);
+		if (index === -1) continue;
+		new_array = new_array.slice(0, index).concat(new_array.slice(index+1));
+	}
+	return new_array;
 };
 
 // Do not edit below this line
