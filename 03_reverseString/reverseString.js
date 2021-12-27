0c7b5e4c2244701a83ea33d81b9926dbcfee89d2
chagstep1 <yandex@chagstep1.ru>
2021-12-27 15:38:43 +0300

    Complete Exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
 const reverseString = function(str) {
-
+	let final_str = "";
+	for (let i = str.length; i > 0; --i)
+	{
+		final_str += str[i-1];
+	}
+	return final_str;
 };
 
 // Do not edit below this line
