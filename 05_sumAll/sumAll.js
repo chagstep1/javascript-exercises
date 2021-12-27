572a85c156bc2e3f5f3862e11b0caad96735ac14
chagstep1 <yandex@chagstep1.ru>
2021-12-27 16:20:42 +0300

    Complete Exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+	if ((typeof num1 !== "number" || num1 < 0) ||
+		(typeof num2 !== "number" || num2 < 0))
+		return "ERROR";
+	
+	let sum = 0;
+	let lesser_num, greater_num;
+	if (num1 < num2)
+	{
+		lesser_num = num1;
+		greater_num = num2;
+	}
+	else
+	{
+		lesser_num = num2;
+		greater_num = num1;
+	}
+	
+	for (let i = lesser_num; i <= greater_num; ++i)
+	{
+		sum += i;
+	}
+	return sum;
 };
 
 // Do not edit below this line
