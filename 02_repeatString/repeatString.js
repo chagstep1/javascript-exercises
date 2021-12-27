0005053b257d3b41225ba1a1f0df390a454013e6
chagstep1 <yandex@chagstep1.ru>
2021-12-27 15:33:00 +0300

    Complete Exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+	if (num < 0) return "ERROR";
+	let end_str = "";
+	for (let i = 0; i < num; ++i)
+	{
+		end_str += string;
+	}
+	return end_str;
 };
 
 // Do not edit below this line
