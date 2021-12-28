a49c70224cb7aab885754bb5d1ec9cbaaea68a8a
chagstep1 <yandex@chagstep1.ru>
2021-12-28 20:38:24 +0300

    Complete Exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = 0;
+	for (num of arr) {
+		sum += num;
+	}
+	return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+	let sum = 1;
+	for (num of arr) {
+		sum *= num;
+	}
+	return sum;
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+	return num ** pow;
 };
 
-const factorial = function() {
-	
+const factorial = function(upToNum) {
+	let result = 1;
+	for (let i = 2; i <= upToNum; ++i)
+	{
+		result *= i;
+	}
+	return result;
 };
 
 // Do not edit below this line
