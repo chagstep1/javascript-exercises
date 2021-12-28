ddf832fb633ec0a9fd2429cf06618380a686b044
chagstep1 <yandex@chagstep1.ru>
2021-12-28 21:11:01 +0300

    Complete Exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+	const date = new Date();
+	let oldest_person = arr[0];
+	let oldest_age = -1;
+	for (person of arr)
+	{
+		let person_age = -1;
+		if ("yearOfDeath" in person) {
+			person_age = person["yearOfDeath"] - person["yearOfBirth"];
+		}
+		else {
+			person_age = date.getFullYear() - person["yearOfBirth"];
+		}
+		
+		if (person_age > oldest_age) {
+			oldest_age = person_age;
+			oldest_person = person;
+		}
+	}
+	return oldest_person;
 };
 
 // Do not edit below this line
