31fb1e19da748435077ea838b6b64f99c773bfab
chagstep1 <yandex@chagstep1.ru>
2021-12-28 20:56:06 +0300

    Complete Exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+	let new_str = str.toLowerCase();
+	new_str = new_str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()\s]/g,"");
+	let heh = "";
+	for (let i = 0; i < (new_str.length/2); ++i) {
+		if (new_str[i] !== new_str[new_str.length-(i+1)]) return false;
+	}
+	return true;
 };
 
 // Do not edit below this line
