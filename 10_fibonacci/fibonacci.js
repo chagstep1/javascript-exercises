36a154e9a824e1e8895a0e6b983d6204880391a7
chagstep1 <yandex@chagstep1.ru>
2021-12-28 21:01:59 +0300

    Complete Exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+	if (num < 0) return "OOPS";
+	let prev_prev_number = 1;
+	let prev_number = 1;
+	let current_number = 1;
+	for (let i = 3; i <= num; ++i)
+	{
+		prev_prev_number = prev_number;
+		prev_number = current_number;
+		current_number = prev_prev_number + prev_number;
+	}
+	return current_number;
 };
 
 // Do not edit below this line
