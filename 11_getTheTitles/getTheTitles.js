6eeabfc06adc494fe6fde3cd4935f13bc888e36e
chagstep1 <yandex@chagstep1.ru>
2021-12-28 21:04:18 +0300

    Complete Exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+	let title_array = [];
+	for (book of arr)
+	{
+		title_array.push(book['title']);
+	}
+	return title_array;
 };
 
 // Do not edit below this line
